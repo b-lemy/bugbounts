021150786d1f1ac5cfd06aebe3306ef8087463f3
b-lemy <lemabrian1234@gmail.com>
2024-02-20 19:11:07 +0300

    fermats

--- crackme.py
+++ crackme.py
@@ -4,9 +4,8 @@
 bezos_cc_secret = "A:4@r%uL`M-^M0c0AbcM-MFE0d_a3hgc3N"
 
 # Reference alphabet
-alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"+ \
-            "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-
+alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ" + \
+           "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 
 
 def decode_secret(secret):
@@ -24,11 +23,14 @@ def decode_secret(secret):
     # decode loop
     for c in secret:
         index = alphabet.find(c)
+        print(index)
         original_index = (index + rotate_const) % len(alphabet)
         decoded = decoded + alphabet[original_index]
 
     print(decoded)
 
+
+decode_secret(bezos_cc_secret)
 
 
 def choose_greatest():
@@ -39,16 +41,15 @@ def choose_greatest():
 
     user_value_1 = input("What's your first number? ")
     user_value_2 = input("What's your second number? ")
-    greatest_value = user_value_1 # need a value to return if 1 & 2 are equal
+    greatest_value = user_value_1  # need a value to return if 1 & 2 are equal
 
     if user_value_1 > user_value_2:
         greatest_value = user_value_1
     elif user_value_1 < user_value_2:
         greatest_value = user_value_2
 
-    print( "The number with largest positive magnitude is "
-        + str(greatest_value) )
-
+    print("The number with largest positive magnitude is "
+          + str(greatest_value))
 
 
 choose_greatest()
