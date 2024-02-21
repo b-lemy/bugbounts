447d1d4d0e5eb523f54c8c58b24e73fa12650b5c
b-lemy <lemabrian1234@gmail.com>
2024-02-21 16:20:34 +0300

    more query snippetss

---
+++ snippets.py
@@ -0,0 +1,18 @@
+# math floor
+# Syntax:
+# math.floor(x)
+# Returns:
+# largest integer not greater than x.
+# math.ceil(x)
+# Returns:
+# smallest integer not less than x
+
+import math
+# prints the ceil using ceil() method
+print("math.ceil(-23.11) : ", math.ceil(-23.11))
+print("math.ceil(300.16) : ", math.ceil(300.16))
+print("math.ceil(300.72) : ", math.ceil(300.72))
+
+print("math.floor(-23.11) : ", math.floor(-23.11))
+print("math.floor(300.16) : ", math.floor(300.16))
+print("math.floor(300.72) : ", math.floor(300.72))
