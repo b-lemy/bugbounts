d4ef465a7ba6bc28fee46c30b4875e0151d6c0e9
b-lemy <lemabrian1234@gmail.com>
2024-04-14 12:48:56 +0300

    data structures and algorithm

--- Fermat's Factorization.py
+++ Fermat's Factorization.py
@@ -2,17 +2,18 @@
 
 from math import floor, sqrt
 
+
 def factorize(n):
     # since even nos. are always divisible by 2, one of the factors will always
     # be 2
     if (n & 1) == 0:
-        return (n/2, 2)
+        return n / 2, 2
 
     a = floor(sqrt(n))
 
     # if n is a perfect square the factors will be ( sqrt(n), sqrt(n) )
     if a * a == n:
-        return (a, a)
+        return a, a
 
     # n = (a - b) * (a + b)
     # n = a^2 - b^2
@@ -21,9 +22,10 @@ def factorize(n):
         a += 1
         _b = a * a - n
         b = int(sqrt(_b))
-        if (b * b == _b):
+        if b * b == _b:
             break
 
-    return (a + b, a - b)
+    return a + b, a - b
+
 
 print(factorize(105327569))
